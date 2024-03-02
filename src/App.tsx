4798d0471ea9034bdca0a0621b1f0c17e19f18ef
Mani Natarajan <nmaniklbs@gmail.com>
2024-03-02 14:15:06 -0500

    Updated some text for testing

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
           Added some more details here for testing
+          Hey super update
         </p>
       </div>
       <p className="read-the-docs">
